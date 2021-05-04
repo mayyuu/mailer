40c961544c63b90c1c8c42694674f3944992e5cd
Mayuri Patil <patilmayuri573@gmail.com>
2021-05-04 17:13:15 +0530

    --Added universal io package. --Replaced the dart:io with universal:io.

--- lib/src/entities/attachment.dart
+++ lib/src/entities/attachment.dart
@@ -1,6 +1,6 @@
 import 'dart:async';
 import 'dart:convert' as convert;
-import 'dart:io';
+import 'package:universal_io/io.dart';
 
 import 'package:mime/mime.dart' as mime;
 import 'package:path/path.dart';
--- lib/src/smtp/smtp_client.dart
+++ lib/src/smtp/smtp_client.dart
@@ -1,6 +1,6 @@
 import 'dart:async';
 import 'dart:convert' as convert;
-import 'dart:io';
+import 'package:universal_io/io.dart';
 
 import 'package:mailer/smtp_server.dart';
 
